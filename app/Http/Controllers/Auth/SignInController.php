e57327f7d4d308b2e5fa432a7e43e8398c581c48
Bahlul Siddiquee <bahlul.tausif@gmail.com>
2024-07-20 22:53:25 -0400

    Having an issue with the login re-direction

---
+++ app/Http/Controllers/Auth/SignInController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Dotenv\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SignInController extends Controller
+{
+    function loginUser(Request $request)
+    {
+        $validation = Validator($request->all(),
+        [
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string'
+        ]);
+
+        if($validation->fails()){
+            return response()->json([
+                'status'=>400,
+                'message' => $validation->errors()->first()
+            ]);
+        }
+        else{
+            $credientials = array(
+                'email'=>$request->email,
+                'password'=>$request->password
+            );
+
+            if(Auth::attempt($credientials,false)){
+
+                if(Auth::User()->hasRole('admin')){
+                    return response()->json([
+                        'status'=>200,
+                        'message' => 'Admin User',
+                        'url'=>'admin/index'
+                    ]);
+                }
+                else{
+                    return response()->json([
+                        'status'=>200,
+                        'message' => 'Non User'
+                    ]);
+                }
+            }
+
+            else{
+                return response()->json([
+                    'status'=>404,
+                    'message' => "Wrong credientials"
+                ]);
+            }
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
-
-use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Auth\SignInController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('admin/index');
+// });
+
+Route::get('/signin', function () {
+    return view('admin/auth/signin');
+});
+
+Route::get('/dashboard', function () {
     return view('admin/index');
 });
 
-Route::get('/createAdmin',[AuthController::class,'adminRole']);
+//Route::get('/createAdmin',[AuthController::class,'adminRole']);
+
+Route::post('/login',[SignInController::class,'loginUser']);
+
